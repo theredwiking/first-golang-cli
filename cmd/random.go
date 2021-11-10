530383b6ce634f458e88eeb980266c499a80a8be
Theredwiking <theredviking1@gmail.com>
2021-11-10 14:18:54 +0100

    Can take flags

--- cmd/random.go
+++ cmd/random.go
@@ -6,6 +6,8 @@ import (
     "io/ioutil"
 	"encoding/json"
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -16,18 +18,33 @@ var randomCmd = &cobra.Command{
 	Short: "Get random dad joke",
 	Long: `This commands fetch random dad joke from icanhazdadjoke api`,
 	Run: func(cmd *cobra.Command, args []string) {
-		getRandomJoke()
+		jokeTerm, _ := cmd.Flags().GetString("term")
+
+		if jokeTerm != "" {
+			getJokeWithTerm(jokeTerm)
+		} else {
+			getRandomJoke()
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(randomCmd)
+
+	randomCmd.PersistentFlags().String("term", "", "Search term for dad joke")
 }
 
 type Joke struct {
 	ID string `json:"id"`
 	Joke string `json:"joke"`
 	Status int `json:"status"`
+}
+
+type SearchResult struct {
+	Results json.RawMessage `json:"results"`
+	SearchTerm string `json:"search_term"`
+	Status int `json:"status"`
+	TotalJokes int `json:"total_jokes"`
 }
 
 func getRandomJoke() {
@@ -40,6 +57,26 @@ func getRandomJoke() {
 	}
 
 	fmt.Println(string(joke.Joke))
+}
+
+func getJokeWithTerm(jokeTerm string) {
+	total, results := getJokeDataTerm(jokeTerm)
+	randomiseJokeList(total, results)
+}
+
+func randomiseJokeList (length int, jokeList []Joke) {
+	rand.Seed(time.Now().Unix())
+
+	min := 0
+	max := length - 1
+
+	if length <= 0 {
+		err := fmt.Errorf("No jokes found with this term")
+		fmt.Println(err.Error())
+	} else {
+		randomNum := min + rand.Intn(max-min)
+		fmt.Println(jokeList[randomNum].Joke)
+	}
 }
 
 func getJokeData(baseAPI string) []byte {
@@ -69,4 +106,21 @@ func getJokeData(baseAPI string) []byte {
 	}
 
 	return responseBytes
+}
+
+func getJokeDataTerm(jokeTerm string) (totalJokes int, jokeList []Joke){
+	url := fmt.Sprintf("https://icanhazdadjoke.com/search?term=%s", jokeTerm)
+	responseBytes := getJokeData(url)
+	jokeListRaw := SearchResult{}
+
+	if err := json.Unmarshal(responseBytes, &jokeListRaw); err != nil {
+		log.Printf("Error unmarshalling responseBytes. %v", err)
+	}
+
+	jokes := []Joke{}
+	if err := json.Unmarshal(jokeListRaw.Results, &jokes); err != nil {
+		log.Printf("Error unmarshalling jokeListRaw. %v", err)
+	}
+
+	return jokeListRaw.TotalJokes, jokes
 }
